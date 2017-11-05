3879207456bd616a4a3f5866d54111ea759cdace
JohnPark <jongmanpark418@gmail.com>
2017-11-05 00:08:55 -0700

    No big change here

--- client/src/components/posts/PostNew.js
+++ client/src/components/posts/PostNew.js
@@ -6,9 +6,12 @@ import PostForm from "./PostForm";
 import * as actions from "../../actions";
 
 class PostNew extends Component {
+  submit = () => {
+    // TODO: Validate this form before post
+    this.props.submitPost(this.props.form.values, this.props.history);
+  };
   render() {
-    const submitPost = this.props.submitPost;
-    return <PostForm onPostSubmit={() => submitPost(this.props.form.values, this.props.history)} />;
+    return <PostForm onPostSubmit={this.submit} />;
   }
 }
 function mapStateToProps(state) {
